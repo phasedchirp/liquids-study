787b580fa97d1b99bd5e579a37a20e7688f57048
sean <sleepypooka@gmail.com>
2016-03-06 11:58:18 -0800

    Updated multinomial model. Works with l and n data, major issues with r, w data.

--- project-binomial.R
+++ project-binomial.R
@@ -2,95 +2,23 @@ source("processing.R")
 library(tidyr)
 library(rethinking)
 
-countsL <- liquidsL %>% mutate(init = ifelse(POSITION=="initial",1,0),
+liquidsL <- liquidsL %>% mutate(init = ifelse(POSITION=="initial",1,0),
                               med = ifelse(POSITION=="medial",1,0),
                               consID = as.numeric(cons),
                               Man = ifelse(lang=="Mandarin",1,0),
                               pID = as.numeric(SUBJECT),
                               vI = ifelse(VOWEL=="i",1,0),
                               vU = ifelse(VOWEL=="u",1,0)) %>%
-  group_by(Man,init,med,vI,vU) %>%
-  summarize(correct=as.integer(sum(CHOICE=='l')),
-            error=sum(CHOICE!='l'),
-            total=as.integer(sum(correct,error)),
-            prop = correct/total) %>%
+  mutate(correct=ifelse(CHOICE=='l',1,0)) %>%
   as.data.frame
 
-
-
-modelL.1 <- map2stan(
-  alist(
-    correct ~ dbinom(total,theta) ,
-    
-    # Linear models:
-    logit(theta) <-  a + bL*Man + pos,
-    # submodel for position
-    pos <- bInit*init + bMed*med,
-    
-    # Priors:
-    # Intercept:
-    a ~ dnorm(0,50),
-    # language Effect
-    bL ~ dnorm(0,10),
-    # position effects:
-    bInit ~ dnorm(0,10),
-    bMed ~ dnorm(0,10)
-  ),
-  data = countsL,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.9)
-)
-
-modelL.2 <- map2stan(
-  alist(
-    correct ~ dbinom(total,theta) ,
-    
-    # Linear models:
-    logit(theta) <-  a + bL*Man + pos,
-    # submodel for position
-    pos <- bInit*init + iInit*init*Man + bMed*med +iMed*med*Man,
-    
-    # Priors:
-    # Intercept:
-    a ~ dnorm(0,50),
-    # language Effect
-    bL ~ dnorm(0,10),
-    # position effects:
-    bInit ~ dnorm(0,10),
-    bMed ~ dnorm(0,10),
-    # position:language interaction:
-    iInit ~ dnorm(0,10),
-    iMed ~ dnorm(0,10)
-  ),
-  data = countsL,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.9)
-)
-
-
-modelL.3 <- map2stan(
-  alist(
-    correct ~ dbinom(total,theta) ,
-    
-    # Linear models:
-    logit(theta) <-  a + bL*Man + pos + vowel,
-    # submodels for position, vowel
-    pos <- bInit*init + iInit*init*Man + bMed*med +iMed*med*Man,
-    vowel <- bI*vI + bU*vU ,
-    # Priors:
-    # Intercept:
-    a ~ dnorm(0,50),
-    # language Effect
-    bL ~ dnorm(0,10),
-    # position effects:
-    bInit ~ dnorm(0,10),
-    bMed ~ dnorm(0,10),
-    # position:language interaction:
-    iInit ~ dnorm(0,10),
-    iMed ~ dnorm(0,10),
-    # vowel main effects:
-    bI ~ dnorm(0,10),
-    bU ~ dnorm(0,10)
-  ),
-  data = countsL,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.9)
-)
-
+countsL <- liquidsL %>% 
+  group_by(lang,POSITION,VOWEL,Man,init,med,vI,vU) %>% 
+  summarize(correct=sum(CHOICE=='l'),
+            error=sum(CHOICE!='l'),
+            total = sum(correct,error),
+            prop = correct/total) %>% as.data.frame %>%
+  select(Man,init,med,vI,vU,correct,error,total)
 
 modelL.4 <- map2stan(
   alist(
@@ -119,18 +47,202 @@ modelL.4 <- map2stan(
     iI ~ dnorm(0,10),
     iU ~ dnorm(0,10)
   ),
+  data = countsL,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.95)
+)
+
+precis(modelL.4)
+
+
+plotFrame <- liquidsL %>% mutate(init = ifelse(POSITION=="initial",1,0),
+                                 med = ifelse(POSITION=="medial",1,0),
+                                 consID = as.numeric(cons),
+                                 Man = ifelse(lang=="Mandarin",1,0),
+                                 pID = as.numeric(SUBJECT),
+                                 vI = ifelse(VOWEL=="i",1,0),
+                                 vU = ifelse(VOWEL=="u",1,0)) %>%
+  group_by(lang,POSITION,VOWEL,Man,init,med,vI,vU) %>% 
+  summarize(correct=sum(CHOICE=='l'),
+            error=sum(CHOICE!='l'),
+            total = sum(correct,error),
+            prop = correct/total) %>%
+  as.data.frame
+
+#newDat <- plotFrame[,c("Man","init","med")]
+
+newDat <- plotFrame[,c("Man","init","med","vI","vU")]
+
+p <- link(modelL.4,data=newDat,n=5000)["theta"]
+mu <- apply(p$theta,2,mean)
+intervals <- apply(p$theta,2,HPDI)
+
+
+
+plotFrame$est <- mu
+plotFrame$upper <- intervals[2,]
+plotFrame$lower <- intervals[1,]
+
+ggplot(data=plotFrame,aes(y=prop,x=POSITION,color=VOWEL,linetype=lang))+
+  geom_point(position=position_dodge(width=0.5),size=4,alpha=0.75)+ #observed
+  geom_point(aes(y=est),position=position_dodge(width=0.5),size=2)+
+  geom_errorbar(aes(ymin=upper,ymax=lower),position=position_dodge(width=0.5),size=1,width=0)+
+  theme_bw()+ggtitle("Response patterns for initial liquids")
+
+
+
+modelL.5 <- map2stan(
+  alist(
+    correct ~ dbinom(total,theta) ,
+    
+    # Linear models:
+    logit(theta) <-  a + bL*Man + pos + vowel + vPos,
+    # submodels for position, vowel, interactions
+    pos <- bInit*init + iInit*init*Man + bMed*med +iMed*med*Man,
+    vowel <- bI*vI + iI*vI*Man + bU*vU +iU*vU*Man ,
+    vPos <- iIinit*vI*init + iUinit*vU*init + iImed*vI*med + iUmed*vU*med,
+    # Priors:
+    # Intercept:
+    a ~ dnorm(0,50),
+    # language Effect
+    bL ~ dnorm(0,5),
+    # position effects:
+    bInit ~ dnorm(0,5),
+    bMed ~ dnorm(0,5),
+    # position:language interaction:
+    iInit ~ dnorm(0,5),
+    iMed ~ dnorm(0,5),
+    # vowel main effects:
+    bI ~ dnorm(0,5),
+    bU ~ dnorm(0,5),
+    # vowel:language interaction:
+    iI ~ dnorm(0,5),
+    iU ~ dnorm(0,5),
+    # position:vowel interaction:
+    iIinit ~ dnorm(0,5),
+    iUinit ~ dnorm(0,5),
+    iImed ~ dnorm(0,5),
+    iUmed ~ dnorm(0,5)
+  ),
   data = countsL,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.9)
 )
 
-
-compare(modelL.1,modelL.2,modelL.3,modelL.4)
-
+compare(modelL.5,modelL.4)
 
 
-# precis(modelL1)
+plotFrame <- liquidsL %>% mutate(init = ifelse(POSITION=="initial",1,0),
+                                 med = ifelse(POSITION=="medial",1,0),
+                                 consID = as.numeric(cons),
+                                 Man = ifelse(lang=="Mandarin",1,0),
+                                 pID = as.numeric(SUBJECT),
+                                 vI = ifelse(VOWEL=="i",1,0),
+                                 vU = ifelse(VOWEL=="u",1,0)) %>%
+  group_by(lang,POSITION,VOWEL,Man,init,med,vI,vU) %>% 
+  summarize(correct=as.integer(sum(CHOICE=='l')),
+            error=sum(CHOICE!='l'),
+            total=as.integer(sum(correct,error)),
+            prop = correct/total) %>%
+  as.data.frame
+
+#newDat <- plotFrame[,c("Man","init","med")]
+
+newDat <- plotFrame[,c("Man","init","med","vI","vU")]
+
+p <- link(modelL.5,countsL,n=5000)["theta"]
+mu <- apply(p$theta,2,mean)
+intervals <- apply(p$theta,2,HPDI)
+
+
+
+plotFrame$est <- mu
+plotFrame$upper <- intervals[2,]
+plotFrame$lower <- intervals[1,]
+
+ggplot(data=plotFrame,aes(y=prop,x=POSITION,color=VOWEL,linetype=lang))+
+  geom_hline(yintercept=0.5,color="red",alpha=0.5,linetype=2,size=1)+
+  geom_point(position=position_dodge(width=0.5),size=4,alpha=0.75)+ #observed
+  geom_point(aes(y=est),position=position_dodge(width=0.5),size=2)+
+  geom_errorbar(aes(ymin=upper,ymax=lower),position=position_dodge(width=0.5),size=1,width=0)+
+  scale_y_continuous(breaks = seq(from=0,to=1,by=0.1),limits=c(0.3,1))+
+  labs(x="Position",y="Proportion correct")+
+  theme_bw()+ggtitle("Proportion correct for /l/ by position, vowel, and language")
+
+################################################################################
+# Alternative model specifications and comparison using WAIC                   #
+################################################################################
 # 
-# # newDat <- countsL[,c("Man","init","med","vI","vU")] %>% distinct(lID,Man,init,med,vI,vU) %>% arrange(lID,Man,med,vI)
-# newDat <- countsL[,c("Man","init","med")] %>% arrange(Man,init,med)
-# p <- link(modelL1,newDat,n=5000)["theta"]
-# mu <- apply(p$theta,2,mean)
-# intervals <- apply(p$theta,2,HPDI)
+# modelL.1 <- map2stan(
+#   alist(
+#     correct ~ dbinom(total,theta) ,
+#     
+#     # Linear models:
+#     logit(theta) <-  a + bL*Man + pos,
+#     # submodel for position
+#     pos <- bInit*init + bMed*med,
+#     
+#     # Priors:
+#     # Intercept:
+#     a ~ dnorm(0,50),
+#     # language Effect
+#     bL ~ dnorm(0,10),
+#     # position effects:
+#     bInit ~ dnorm(0,10),
+#     bMed ~ dnorm(0,10)
+#   ),
+#   data = countsL,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.9)
+# )
+# 
+# modelL.2 <- map2stan(
+#   alist(
+#     correct ~ dbinom(total,theta) ,
+#     
+#     # Linear models:
+#     logit(theta) <-  a + bL*Man + pos,
+#     # submodel for position
+#     pos <- bInit*init + iInit*init*Man + bMed*med +iMed*med*Man,
+#     
+#     # Priors:
+#     # Intercept:
+#     a ~ dnorm(0,50),
+#     # language Effect
+#     bL ~ dnorm(0,10),
+#     # position effects:
+#     bInit ~ dnorm(0,10),
+#     bMed ~ dnorm(0,10),
+#     # position:language interaction:
+#     iInit ~ dnorm(0,10),
+#     iMed ~ dnorm(0,10)
+#   ),
+#   data = countsL,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.9)
+# )
+# 
+# 
+# modelL.3 <- map2stan(
+#   alist(
+#     correct ~ dbinom(total,theta) ,
+#     
+#     # Linear models:
+#     logit(theta) <-  a + bL*Man + pos + vowel,
+#     # submodels for position, vowel
+#     pos <- bInit*init + iInit*init*Man + bMed*med +iMed*med*Man,
+#     vowel <- bI*vI + bU*vU ,
+#     # Priors:
+#     # Intercept:
+#     a ~ dnorm(0,50),
+#     # language Effect
+#     bL ~ dnorm(0,10),
+#     # position effects:
+#     bInit ~ dnorm(0,10),
+#     bMed ~ dnorm(0,10),
+#     # position:language interaction:
+#     iInit ~ dnorm(0,10),
+#     iMed ~ dnorm(0,10),
+#     # vowel main effects:
+#     bI ~ dnorm(0,10),
+#     bU ~ dnorm(0,10)
+#   ),
+#   data = countsL,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.9)
+# )
+# 
+# compare(modelL.1,modelL.2,modelL.3,modelL.4)
+
+
+
--- project.R
+++ project.R
@@ -23,72 +23,74 @@ counts <- liquidsAll %>% mutate(init = ifelse(POSITION=="initial",1,0),
   summarize(l=sum(CHOICE=='l'),
             n=sum(CHOICE=='n'),
             r=sum(CHOICE=='r'),
+            w=sum(CHOICE=='w'),
+            total=sum(l,n,r,w)) %>%
+  as.data.frame
+
+
+counts <- liquidsAll %>%
+  filter(LIQUID == "L") %>%
+  mutate(init = ifelse(POSITION=="initial",1,0),
+         med = ifelse(POSITION=="medial",1,0),
+         lID = as.numeric(LIQUID),
+         Man = ifelse(lang=="Mandarin",1,0),
+         pID = as.numeric(SUBJECT),
+         vI = ifelse(VOWEL=="i",1,0),
+         vU = ifelse(VOWEL=="u",1,0)) %>%
+  group_by(Man,init,med) %>%
+  summarize(l=sum(CHOICE=='l'),
+            n=sum(CHOICE=='n'),
+            r=sum(CHOICE=='r'),
             w=sum(CHOICE=='w'),total=sum(l,n,r,w)) %>%
   as.data.frame
 
-modelL1 <- map2stan(
+model.1 <- map2stan(
   alist(
     l ~ dpois(lambda_l) ,
     n ~ dpois(lambda_n) ,
     r ~ dpois(lambda_r) ,
     w ~ dpois(lambda_w) ,
     # Linear models:
-    log(lambda_l) <-  liq_l + pos_l + v_l, #
-    log(lambda_n) <-  liq_n + pos_n + v_n, #
-    log(lambda_r) <-  liq_r + pos_r + v_r, #
-    log(lambda_w) <-  liq_w + pos_w + v_w, #
-    # Intercepts by liquid in stimulus (also main effect(s) of language):
-    liq_l <- bL_l[lID] + iL_l[lID]*Man,
-    liq_n <- bL_n[lID] + iL_n[lID]*Man,
-    liq_r <- bL_r[lID] + iL_r[lID]*Man,
-    liq_w <- bL_w[lID] + iL_w[lID]*Man,
-    # Position effects and interactions with language (final is intercept):
-    pos_l <- bInit_l*init + bMed_l*med + iInit_l*Man*init + iMed_l*Man*med,
-    pos_n <- bInit_n*init + bMed_n*med + iInit_n*Man*init + iMed_n*Man*med,
-    pos_r <- bInit_r*init + bMed_r*med + iInit_r*Man*init + iMed_r*Man*med,
-    pos_w <- bInit_w*init + bMed_w*med + iInit_w*Man*init + iMed_w*Man*med,
-    # Vowel effects and interactions with language:
-    v_l <- bI_l*vI + bU_l*vU + iI_l*vI*Man + iU_l*vU*Man,
-    v_n <- bI_n*vI + bU_n*vU + iI_n*vI*Man + iU_n*vU*Man,
-    v_r <- bI_r*vI + bU_r*vU + iI_r*vI*Man + iU_r*vU*Man,
-    v_w <- bI_w*vI + bU_w*vU + iI_w*vI*Man + iU_w*vU*Man,
-    # Priors:
-    # Intercepts by liquid:
-    bL_l[lID] ~ dnorm(0,50),
-    bL_n[lID] ~ dnorm(0,50),
-    bL_r[lID] ~ dnorm(0,50),
-    bL_w[lID] ~ dnorm(0,50),
-    # language main effects by liquid:
-    iL_l[lID] ~ dnorm(0,20),
-    iL_n[lID] ~ dnorm(0,20),
-    iL_r[lID] ~ dnorm(0,20),
-    iL_w[lID] ~ dnorm(0,20),
+    log(lambda_l) <-  a_l + pos_l + bMan_l*Man, #+ v_l, #
+    log(lambda_n) <-  a_n + pos_n + bMan_n*Man , #+ v_n, #
+    log(lambda_r) <-  a_r + pos_r + bMan_r*Man , #+ v_r, #
+    log(lambda_w) <-  a_w + pos_w + bMan_w*Man , #+ v_w, #
     # Position:
-    c(bInit_l, bInit_n, bInit_r, bInit_w) ~ dnorm(0,10),
-    c(bMed_l,bMed_n,bMed_r,bMed_w) ~ dnorm(0,10),
+    pos_l <- bInit_l*init + bMed_l*med,
+    pos_n <- bInit_n*init + bMed_n*med,
+    pos_r <- bInit_r*init + bMed_r*med,
+    pos_w <- bInit_w*init + bMed_w*med,
+    # Intercepts:
+    c(a_l,a_n,a_r,a_w) ~ dnorm(0,50),
+    # Position effects:
+    c(bInit_l, bInit_n, bInit_r, bInit_w) ~ dnorm(0,5),
+    c(bMed_l, bMed_n, bMed_r, bMed_w) ~ dnorm(0,5),
+    # Language effects:
+    c(bMan_l, bMan_n, bMan_r, bMan_w) ~ dnorm(0,5)
     
-    # language:position interactions:
-    c(iInit_l, iInit_n, iInit_r, iInit_w) ~ dnorm(0,10),
-    c(iMed_l,iMed_n,iMed_r,iMed_w) ~ dnorm(0,10),
-
-    # vowel
-    c(bI_l, bI_n, bI_r, bI_w) ~ dnorm(0,10),
-    c(bU_l, bU_n, bU_r, bU_w) ~ dnorm(0,10),
-    
-    # language:vowel interactions:
-    c(iI_l, iI_n, iI_r, iI_w) ~ dnorm(0,10),
-    c(iU_l, iU_n, iU_r, iU_w) ~ dnorm(0,10)
-    
-    # position:vowel interactions:
   ),
   data = counts,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.9)
 )
 
-precis(modelL,depth=2)
-plot(precis(modelL,depth=2))
 
-newDat <- counts[,c("lID","Man","init","med","vI","vU")] %>% distinct(lID,Man,init,med,vI,vU) %>% arrange(lID,Man,med,vI)
-p <- link(modelL1,newDat,n=5000)[c("lambda_l","lambda_n","lambda_r","lambda_w")]
+precis(model.1,depth=2)
+plotFrame <- liquidsAll %>%
+  filter(LIQUID=="L") %>%
+  mutate(init = ifelse(POSITION=="initial",1,0),
+         med = ifelse(POSITION=="medial",1,0),
+         consID = as.numeric(cons),
+         lID = as.numeric(LIQUID),
+         Man = ifelse(lang=="Mandarin",1,0)) %>%
+  group_by(lang,POSITION,Man,init,med) %>%#,POSITION,VOWEL) %>%
+  summarize(l=sum(CHOICE=='l'),
+            n=sum(CHOICE=='n'),
+            r=sum(CHOICE=='r'),
+            w=sum(CHOICE=='w'),total=n()) %>%
+  as.data.frame
+
+
+newDat <- plotFrame[,c("Man","init","med")]
+p <- link(modelL,newDat,n=5000)[c("lambda_l","lambda_n","lambda_r","lambda_w")]
 
 # See McElreath (2016) p.327
 pSums <- with(p,lambda_l+lambda_n+lambda_r+lambda_w)
@@ -96,16 +98,7 @@ props <- lapply(p,function(x){x/pSums})
 mu <- lapply(props,apply,2,median)
 intervals <- lapply(props,apply,2,function(x){HPDI(x,prob=0.99)})
 
-plotFrame <- liquidsAll %>% mutate(init = ifelse(POSITION=="initial",1,0),
-                                    med = ifelse(POSITION=="medial",1,0),
-                                    consID = as.numeric(cons),
-                                    lID = as.numeric(LIQUID),
-                                    Man = ifelse(lang=="Mandarin",1,0)) %>%
-  group_by(LIQUID,lang,POSITION,VOWEL) %>%
-  summarize(l=sum(CHOICE=='l'),
-            n=sum(CHOICE=='n'),
-            r=sum(CHOICE=='r'),
-            w=sum(CHOICE=='w'),total=n()) %>%
+plotFrame <- plotFrame %>%
   gather(choice,count,l:w) %>%
   mutate(obs = count/total)%>%
   as.data.frame
@@ -114,110 +107,9 @@ plotFrame$est <- c(mu$lambda_l, mu$lambda_n, mu$lambda_r, mu$lambda_w)
 plotFrame$upper <- with(intervals,c(lambda_l[2,], lambda_n[2,], lambda_r[2,], lambda_w[2,]))
 plotFrame$lower <- with(intervals,c(lambda_l[1,], lambda_n[1,], lambda_r[1,], lambda_w[1,]))
 
-initial <- plotFrame %>% subset(POSITION=="initial")
-ggplot(data=initial,aes(y=obs,x=VOWEL,col=choice))+
-  geom_point(position=position_dodge(width=0.5),size=4,alpha=0.75)+ #observed
-  geom_point(aes(y=est),position=position_dodge(width=0.5),size=2)+
-  geom_errorbar(aes(ymin=upper,ymax=lower),position=position_dodge(width=0.5),size=1,width=0)+
-  theme_bw()+facet_grid(LIQUID~lang)+ggtitle("Response patterns for initial liquids")
-
-medial <- plotFrame %>% subset(POSITION=="medial")
-ggplot(data=medial,aes(y=obs,x=VOWEL,col=choice))+
-  geom_point(position=position_dodge(width=0.5),size=4,alpha=0.75)+ #observed
-  geom_point(aes(y=est),position=position_dodge(width=0.5),size=2)+
-  geom_errorbar(aes(ymin=upper,ymax=lower),position=position_dodge(width=0.5),size=1,width=0)+
-  theme_bw()+facet_grid(LIQUID~lang)+ggtitle("Response patterns for medial liquids")
-
-final <- plotFrame %>% subset(POSITION=="final")
-ggplot(data=final,aes(y=obs,x=VOWEL,col=choice))+
-  geom_point(position=position_dodge(width=0.5),size=4,alpha=0.75)+ #observed
-  geom_point(aes(y=est),position=position_dodge(width=0.5),size=2)+
-  geom_errorbar(aes(ymin=upper,ymax=lower),position=position_dodge(width=0.5),size=1,width=0)+
-  theme_bw()+facet_grid(LIQUID~lang)+ggtitle("Response patterns for final liquids")
-
-
-
-
-modelL2 <- map2stan(
-  alist(
-    l ~ dpois(lambda_l) ,
-    n ~ dpois(lambda_n) ,
-    r ~ dpois(lambda_r) ,
-    w ~ dpois(lambda_w) ,
-    # Linear models:
-    log(lambda_l) <-  liq_l + pos_l, #
-    log(lambda_n) <-  liq_n + pos_n, #
-    log(lambda_r) <-  liq_r + pos_r, #
-    log(lambda_w) <-  liq_w + pos_w, #
-    # Intercepts by liquid in stimulus (also main effect(s) of language):
-    liq_l <- bL_l[lID] + iL_l[lID]*Man,
-    liq_n <- bL_n[lID] + iL_n[lID]*Man,
-    liq_r <- bL_r[lID] + iL_r[lID]*Man,
-    liq_w <- bL_w[lID] + iL_w[lID]*Man,
-    # Position effects and interactions with language (final is intercept):
-    pos_l <- bInit_l*init + bMed_l*med + iInit_l*Man*init + iMed_l*Man*med,
-    pos_n <- bInit_n*init + bMed_n*med + iInit_n*Man*init + iMed_n*Man*med,
-    pos_r <- bInit_r*init + bMed_r*med + iInit_r*Man*init + iMed_r*Man*med,
-    pos_w <- bInit_w*init + bMed_w*med + iInit_w*Man*init + iMed_w*Man*med,
-    # Priors:
-    # Intercepts by liquid:
-    bL_l[lID] ~ dnorm(0,10),
-    bL_n[lID] ~ dnorm(0,10),
-    bL_r[lID] ~ dnorm(0,10),
-    bL_w[lID] ~ dnorm(0,10),
-    # language main effects by liquid:
-    iL_l[lID] ~ dnorm(0,10),
-    iL_n[lID] ~ dnorm(0,10),
-    iL_r[lID] ~ dnorm(0,10),
-    iL_w[lID] ~ dnorm(0,10),
-    # Position:
-    c(bInit_l, bInit_n, bInit_r, bInit_w) ~ dnorm(0,10),
-    c(bMed_l,bMed_n,bMed_r,bMed_w) ~ dnorm(0,10),
-    
-    # language:position interactions:
-    c(iInit_l, iInit_n, iInit_r, iInit_w) ~ dnorm(0,10),
-    c(iMed_l,iMed_n,iMed_r,iMed_w) ~ dnorm(0,10)
-  ),
-  data = counts,iter=5000,cores=4,chains=4, control=list(max_treedepth=15,adapt_delta=0.9)
-)
-
-newDat <- counts[,c("lID","Man","init","med")] %>% distinct(lID,Man,init,med) %>% arrange(lID,Man,med)
-p <- link(modelL2,newDat)[c("lambda_l","lambda_n","lambda_r","lambda_w")]
-
-# See McElreath (2016) p.327
-pSums <- with(p,lambda_l+lambda_n+lambda_r+lambda_w)
-props <- lapply(p,function(x){x/pSums})
-mu <- lapply(props,apply,2,median)
-intervals <- lapply(props,apply,2,function(x){HPDI(x,prob=0.99)})
-
-
-mu <- lapply(p,apply,2,median)
-pSums <- with(mu,lambda_l+lambda_n+lambda_r+lambda_w)
-mu <- lapply(mu,function(x){x/pSums})
-intervals <- lapply(p,apply,2,function(x){HPDI(x,prob=0.99)})
-pSums <- with(intervals,lambda_l+lambda_n+lambda_r+lambda_w)
-intervals <- lapply(intervals,function(x){x/pSums})
-
-plotFrame <- liquidsAll %>% mutate(init = ifelse(POSITION=="initial",1,0),
-                                   med = ifelse(POSITION=="medial",1,0),
-                                   consID = as.numeric(cons),
-                                   lID = as.numeric(LIQUID),
-                                   Man = ifelse(lang=="Mandarin",1,0)) %>%
-  group_by(LIQUID,lang,POSITION) %>%
-  summarize(l=sum(CHOICE=='l'),
-            n=sum(CHOICE=='n'),
-            r=sum(CHOICE=='r'),
-            w=sum(CHOICE=='w'),total=n()) %>%
-  gather(choice,count,l:w) %>%
-  mutate(obs = count/total)%>%
-  as.data.frame
-
-plotFrame$est <- c(mu$lambda_l, mu$lambda_n, mu$lambda_r, mu$lambda_w)
-plotFrame$upper <- with(intervals,c(lambda_l[2,], lambda_n[2,], lambda_r[2,], lambda_w[2,]))
-plotFrame$lower <- with(intervals,c(lambda_l[1,], lambda_n[1,], lambda_r[1,], lambda_w[1,]))
 
 ggplot(data=plotFrame,aes(y=obs,x=lang,col=choice))+
   geom_point(position=position_dodge(width=0.5),size=4,alpha=0.75)+ #observed
   geom_point(aes(y=est),position=position_dodge(width=0.5),size=2)+
   geom_errorbar(aes(ymin=upper,ymax=lower),position=position_dodge(width=0.5),size=1,width=0)+
-  theme_bw()+facet_grid(LIQUID~POSITION)
+  theme_bw()+facet_grid(.~POSITION)
